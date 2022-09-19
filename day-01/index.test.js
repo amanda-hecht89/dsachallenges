bc2bc279ef7be67016b732c9c50cb6eb92222efa
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-19 16:53:23 -0600

    HOF01 test failing

--- day-01/index.test.js
+++ day-01/index.test.js
@@ -5,5 +5,18 @@ describe('primitive code challenges', () => {
     it('should reverse a sentence', () => {
       expect(reverseSentence('alchemy rocks gold')).toBe('ymehcla skcor dlog');
     });
+
+    it('should add puncuation to end of the sentence', () => {
+      const addExcitment = addPunctuation('!!!');
+      const addQuery = addPunctuation('???');
+      const awesome = addExcitment('Amanda Rocks');
+      expect(awesome).toEqual('Amanda Rocks!!!');
+      expect(addQuery('What did you say?')).toEqual('What did you say??');
+    });
+
+
+
+
+
   });
 });
