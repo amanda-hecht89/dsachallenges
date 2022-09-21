1a158632921024b1ec7b076b6ab1ec5d7c4c2f27
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-21 12:15:49 -0600

    Stack test failing

--- day-01/index.test.js
+++ day-01/index.test.js
@@ -2,12 +2,12 @@ const { reverseSentence, addPunctuation, titleCaseWord, oddOrEven, arrayAt, anag
 
 describe('primitive code challenges', () => {
   describe('reverseSentence', () => {
-    it('should reverse a sentence', () => {
+    it.skip('should reverse a sentence', () => {
       expect(reverseSentence('alchemy rocks gold')).toBe('ymehcla skcor dlog');
     });
 
 
-    it('should add puncuation to end of the sentence', () => {
+    it.skip('should add puncuation to end of the sentence', () => {
       const addExcitment = addPunctuation('!!!');
       const addQuery = addPunctuation('???');
       const awesome = addExcitment('Amanda Rocks');
@@ -16,23 +16,23 @@ describe('primitive code challenges', () => {
     });
 
 
-    it('should title case words', () => {
+    it.skip('should title case words', () => {
       expect(titleCaseWord('amANda iS amaZing')).toEqual('Amanda Is Amazing');
     });
 
 
-    it('should determine even or odd', () => {
+    it.skip('should determine even or odd', () => {
       expect(oddOrEven(40)).toEqual('even');
       expect(oddOrEven(41)).toBe('odd');
     });
 
-    it('should find array.at', () => {
+    it.skip('should find array.at', () => {
       const arr = [5, 6, 7, 8];
       expect(arrayAt(arr, 2)).toEqual(7);
       expect(arrayAt(arr, -1)).toEqual(8);
     });
 
-    it('should return true false for anagram', () => {
+    it.skip('should return true false for anagram', () => {
       const first = 'debit card';
       const second = 'bad credit';
       const third = 'banana';
@@ -42,13 +42,13 @@ describe('primitive code challenges', () => {
       expect(anagrams(third, fourth)).toEqual(false);
     });
 
-    it('should replace certain numbers with the words fizz and buzz in an array', () => {
+    it.skip('should replace certain numbers with the words fizz and buzz in an array', () => {
       const fizzBuzzArray = [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 
         11, 'fizz', 13, 14, 'fizzBuzz'];
       expect(fizzBuzz(15)).toEqual(fizzBuzzArray);
     });
 
-    it('should takes a number and returns an array that contains, all of the numbers from 1 to 50 that are multiples of said number', () => {
+    it.skip('should takes a number and returns an array that contains, all of the numbers from 1 to 50 that are multiples of said number', () => {
       expect(multiple(5)).toEqual([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
       expect(multiple(10)).toEqual([10, 20, 30, 40, 50]);
       expect(multiple(15)).toEqual([15, 30, 45]);
@@ -56,7 +56,7 @@ describe('primitive code challenges', () => {
       expect(multiple(25)).toEqual([25, 50]);
     });
 
-    it(' rootDigit should return a single digit sum of number', () => {
+    it.skip(' rootDigit should return a single digit sum of number', () => {
       expect(rootDigit(123)).toEqual(6);
       expect(rootDigit(4322)).toEqual(2);
       expect(rootDigit(999888777)).toEqual(9);
