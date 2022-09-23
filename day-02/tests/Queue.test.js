8e48008acaed25e590e1b7f53638039db843a652
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-23 12:06:12 -0600

    queue test 2 passing, first one was first test passing

--- day-02/tests/Queue.test.js
+++ day-02/tests/Queue.test.js
@@ -2,8 +2,16 @@ const { Queue } = require('../Queue.js');
 
 describe('Queue', () => {
   it('should add item wuth queue.enqueue', () => {
-const queue = new Queue();
-queue.enqueue('amanda');
-expect(queue.hasNext()).toEqual(true);
+    const queue = new Queue();
+    queue.enqueue('amanda');
+    expect(queue.hasNext()).toEqual(true);
+  });
+
+  it('should remove first item with queue.dequeue', () => {
+    const queue = new Queue();
+    queue.enqueue('hello');
+    expect(queue.hasNext()).toEqual(true);
+    queue.dequeue();
+    expect(queue.hasNext()).toEqual(false);
   });
 });
