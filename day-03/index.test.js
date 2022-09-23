5c1f95ed22dc6e2ab5c04aa52dfe4060abfb7bd3
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-23 11:09:26 -0600

    CH02 test failing

--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,7 +1,8 @@
 const { BinaryTreeNode } = require('./index');
+const { PersonTreeNode } = require('./personIndex');
 
 describe('node based data structures', () => {
-  it('add to bianary node', () => {
+  it.skip('add to bianary node', () => {
     const B = new BinaryTreeNode('B');
     const A = new BinaryTreeNode('A');
     const C = new BinaryTreeNode('C');
@@ -13,5 +14,25 @@ describe('node based data structures', () => {
     expect.any(Function), 'data': 'D', 'left': 
     { 'addBinTree': expect.any(Function), 
       'data': 'C', 'left': null, 'right': null }, 'right': null });
+  });
+
+  it('find a person in the bianary node', () => {
+    const sam = new PersonTreeNode('Sam');
+    const o = new PersonTreeNode(0);
+    const dean = new PersonTreeNode('Dean');
+    const bobby = new PersonTreeNode('Bobby');
+
+    sam.addPeopleTree(o);
+    sam.addPeopleTree(bobby);
+    sam.addPeopleTree(dean);
+
+    expect(sam.findPerson('Sam')).toEqual({
+      'addPeopleTree': expect.any(Function), 
+      'findPerson': expect.any(Function),
+      data: 'Sam',
+      'left': expect.any(Object),
+      'right': expect.any(Object)
+    });
+    expect(sam.findPerson('Castiel')).toEqual(null);
   });
 });
---
+++ day-03/personIndex.js
@@ -0,0 +1,19 @@
+class PersonTreeNode {
+
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
+  
+  add(node) {
+    
+  }
+  
+  findPerson(name) {
+
+  }
+}
+
+module.exports = { PersonTreeNode };
